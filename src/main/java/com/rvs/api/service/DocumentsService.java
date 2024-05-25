eb422d39c4c248ae3ad2f364a37403d3129f9bfa
Roman <roomen817@gmail.com>
2024-05-25 23:23:56 +0200

    + documents managing, uploading/downloading

--- src/main/java/com/rvs/api/service/DocumentsService.java
+++ src/main/java/com/rvs/api/service/DocumentsService.java
@@ -7,17 +7,23 @@ import com.rvs.api.repository.DocumentsRepository;
 import org.docx4j.Docx4J;
 import org.docx4j.convert.out.pdf.PdfConversion;
 import org.docx4j.convert.out.pdf.viaXSLFO.Conversion;
+import org.docx4j.fonts.Mapper;
+import org.docx4j.fonts.PhysicalFont;
+import org.docx4j.fonts.PhysicalFonts;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.fonts.BestMatchingMapper;
+import org.docx4j.wml.Fonts;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.UUID;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -48,21 +54,6 @@ public class DocumentsService {
         return document;
     }
 
-    private byte[] convertDocToPdf(byte[] docxBytes) {
-        try {
-            InputStream inputStream = new ByteArrayInputStream(docxBytes);
-            WordprocessingMLPackage wordMLPackage = Docx4J.load(inputStream);
-
-//            PdfConversion conversion = new Conversion(wordMLPackage);
-//            conversion.output(pdfOutputStream, null);
-
-            ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
-            Docx4J.toPDF(wordMLPackage, pdfOutputStream);
-            return pdfOutputStream.toByteArray();
-        } catch (Docx4JException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     public Document save(Document document){
         return documentsRepository.save(document);
@@ -80,5 +71,29 @@ public class DocumentsService {
                 .findById(id)
                 .map(DocumentDownloadDTO::mapToDTO)
                 .orElseThrow(() -> new NoSuchElementException("A document with id %s was not found".formatted(id)));
+    }
+
+    private byte[] convertDocToPdf(byte[] docxBytes) {
+        try {
+            InputStream inputStream = new ByteArrayInputStream(docxBytes);
+            WordprocessingMLPackage wordMLPackage = Docx4J.load(inputStream);
+
+            URI fontUri = Objects.requireNonNull(DocumentsService.class.getResource("/fonts/e-ukraine-regular_w.ttf")).toURI();
+            PhysicalFonts.addPhysicalFonts("e-ukraine-regular_w", fontUri);
+
+            BestMatchingMapper fontMapper = new BestMatchingMapper();
+            PhysicalFonts.discoverPhysicalFonts();
+            for (PhysicalFont font : PhysicalFonts.getPhysicalFont("e-ukraine-regular_w", fontUri)) {
+                fontMapper.put(font.getName(), font);
+            }
+
+            wordMLPackage.setFontMapper(fontMapper);
+
+            ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
+            Docx4J.toPDF(wordMLPackage, pdfOutputStream);
+            return pdfOutputStream.toByteArray();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
